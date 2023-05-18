b67921b2fd285e3796c7f968884b074f75540958
Maelikah <eva.longoria@gmail.com>
2023-05-18 11:04:46 -0600

    Created Switch case to choose shape rendering

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const responses = require("./lib/createLogo");
+const Circle = require("./lib/Circle");
+const Square = require("./lib/Square");
+const Triangle = require("./lib/Triangle");
+const InvTriange = require("./lib/InvTriangle");
+const Ellipse = require("./lib/Ellipse");
 
 console.log("Welcome to the SVG Logo Generator!");
 console.log("Pleae provide the following information:");
